39ce057249c26f2ceed7841e224b8fc87fc09f99
ggobato <gabrielgobaods@gmail.com>
2025-05-13 17:14:28 -0300

    produto e fpg

--- src/main/java/org/example/entities/Produto.java
+++ src/main/java/org/example/entities/Produto.java
@@ -20,14 +20,46 @@ public class Produto implements Serializable {
     @Column(name = "PRO_PRECO_VENDA", precision = 10, scale = 2)
     private Double proPrecoVenda;
 
+    @Column(name = "PRO_QTDE_ESTOQUE", precision = 10, scale = 2)
+    private Long proQtdeEstoque;
+
+    @Column(name = "PRO_CATEGORIA", precision = 10, scale = 2)
+    private String proCategoria;
+
+    @Column(name = "PRO_CODIGO_BARRAS", precision = 10, scale = 2)
+    private String proCodigoBarras;
+
+    @Column(name = "PRO_MARCA", precision = 10, scale = 2)
+    private String proMarca;
+
+    @Column(name = "PRO_UNIDADE_MEDIDA", precision = 10, scale = 2)
+    private String proUnidadeMedida;
+
+    @Column(name = "PRO_STATUS", precision = 10, scale = 2)
+    private String proStatus;
+
+    @Column(name = "PRO_DATA_CADASTRO", precision = 10, scale = 2)
+    private String proDataCadastro;
+
+    @Column(name = "PRO_DATA_UPDATE", precision = 10, scale = 2)
+    private String proDataUpdate;
+
     public Produto() {
     }
 
-    public Produto(Long proId, String proNome, Double proPrecoCusto, Double proPrecoVenda) {
+    public Produto(Long proId, String proNome, Double proPrecoCusto, Double proPrecoVenda, Long proQtdeEstoque, String proCategoria, String proCodigoBarras, String proMarca, String proUnidadeMedida, String proStatus, String proDataCadastro, String proDataUpdate) {
         this.proId = proId;
         this.proNome = proNome;
         this.proPrecoCusto = proPrecoCusto;
         this.proPrecoVenda = proPrecoVenda;
+        this.proQtdeEstoque = proQtdeEstoque;
+        this.proCategoria = proCategoria;
+        this.proCodigoBarras = proCodigoBarras;
+        this.proMarca = proMarca;
+        this.proUnidadeMedida = proUnidadeMedida;
+        this.proStatus = proStatus;
+        this.proDataCadastro = proDataCadastro;
+        this.proDataUpdate = proDataUpdate;
     }
 
     public Long getProId() {
@@ -60,5 +92,69 @@ public class Produto implements Serializable {
 
     public void setProPrecoVenda(Double proPrecoVenda) {
         this.proPrecoVenda = proPrecoVenda;
+    }
+
+    public Long getProQtdeEstoque() {
+        return proQtdeEstoque;
+    }
+
+    public void setProQtdeEstoque(Long proQtdeEstoque) {
+        this.proQtdeEstoque = proQtdeEstoque;
+    }
+
+    public String getProCategoria() {
+        return proCategoria;
+    }
+
+    public void setProCategoria(String proCategoria) {
+        this.proCategoria = proCategoria;
+    }
+
+    public String getProCodigoBarras() {
+        return proCodigoBarras;
+    }
+
+    public void setProCodigoBarras(String proCodigoBarras) {
+        this.proCodigoBarras = proCodigoBarras;
+    }
+
+    public String getProMarca() {
+        return proMarca;
+    }
+
+    public void setProMarca(String proMarca) {
+        this.proMarca = proMarca;
+    }
+
+    public String getProUnidadeMedida() {
+        return proUnidadeMedida;
+    }
+
+    public void setProUnidadeMedida(String proUnidadeMedida) {
+        this.proUnidadeMedida = proUnidadeMedida;
+    }
+
+    public String getProStatus() {
+        return proStatus;
+    }
+
+    public void setProStatus(String proStatus) {
+        this.proStatus = proStatus;
+    }
+
+    public String getProDataCadastro() {
+        return proDataCadastro;
+    }
+
+    public void setProDataCadastro(String proDataCadastro) {
+        this.proDataCadastro = proDataCadastro;
+    }
+
+    public String getProDataUpdate() {
+        return proDataUpdate;
+    }
+
+    public void setProDataUpdate(String proDataUpdate) {
+        this.proDataUpdate = proDataUpdate;
     }
 }
--- src/main/java/org/example/services/ProdutoService.java
+++ src/main/java/org/example/services/ProdutoService.java
@@ -37,6 +37,14 @@ public class ProdutoService {
             produtoSistema.setProNome(produto.getProNome());
             produtoSistema.setProPrecoCusto(produto.getProPrecoCusto());
             produtoSistema.setProPrecoVenda(produto.getProPrecoVenda());
+            produtoSistema.setProQtdeEstoque(produto.getProQtdeEstoque());
+            produtoSistema.setProCategoria(produto.getProCategoria());
+            produtoSistema.setProCodigoBarras(produto.getProCodigoBarras());
+            produtoSistema.setProMarca(produto.getProMarca());
+            produtoSistema.setProUnidadeMedida(produto.getProUnidadeMedida());
+            produtoSistema.setProStatus(produto.getProStatus());
+            produtoSistema.setProDataCadastro(produto.getProDataCadastro());
+            produtoSistema.setProDataUpdate(produto.getProDataUpdate());
             repository.save(produtoSistema);
             return true;
         }

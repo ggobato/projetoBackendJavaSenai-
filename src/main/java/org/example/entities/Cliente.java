7d97e61ccf2fcca06bd17a156183fb22f99f5dae
ggobato <gabrielgobatods@gmail.com>
2025-05-20 17:16:03 -0300

    att backend

--- src/main/java/org/example/entities/Cliente.java
+++ src/main/java/org/example/entities/Cliente.java
@@ -1,6 +1,11 @@
 package org.example.entities;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -11,16 +16,24 @@ public class Cliente implements Serializable {
     @Column(name = "CLI_ID")
     private Long cliId;
 
-    @Column(name = "CLI_NOME")
+    @Size(max = 100, message = "Nome deve ter no máximo 100 caracteres")
+    @Column(name = "CLI_NOME", nullable = false, length = 100)
     private String cliNome;
 
-    @Column(name = "CLI_CPF", length = 11)
+    @NotBlank(message = "CPF é obrigatória")
+    @CPF(message = "CPF inválido")
+    @Column(name = "CLI_CPF", nullable = false, unique = true, length = 11)
     private String cliCpf;
 
-    @Column(name = "CLI_EMAIL")
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    @Size(max = 100, message = "Email deve ter no máximo 100 caracteres")
+    @Column(name = "CLI_EMAIL", nullable = false, length = 100)
     private String cliEmail;
 
-    @Column(name = "CLI_TELEFONE", length = 14)
+    @NotBlank(message = "Telefone é obrigatório")
+    @Size(max = 14, message = "Telefone deve ter no máximo 14 caracteres")
+    @Column(name = "CLI_TELEFONE", nullable = false, length = 14)
     private String cliTelefone;
 
     public Cliente() {
